0fcf4e7cb090f0f3372af252a7f6468830785da2
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2024-03-26 14:56:26 +0100

    start

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { render } from 'preact'
+import { App } from './components/app'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
